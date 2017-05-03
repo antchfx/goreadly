23a8dd4147dc4df0dc5547332b173f13dd74bb13
zhengchun <zhengchunster@gmail.com>
2017-05-03 15:53:07 +0800

    example case

---
+++ example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/antchfx/readability"
+)
+
+func main() {
+	doc, err := readability.LoadURL(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(doc.Title())
+	fmt.Println(doc.Content())
+}
